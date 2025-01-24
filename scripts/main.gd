04637a8c5da73c5d7a98dff01db0a008e95f24ee
Shyam Modi <shyammodi1729@gmail.com>
2025-01-25 00:46:29 +0530

    Level1 almost complete

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,17 +1,16 @@
-# In Main Script
 extends Node
-
 var max_enemies: int
 var level: int
 var lives: int
 var current_enemies: int
-signal player_damaged  # New signal for damage
+var can_transition: bool = false
+signal player_damaged
 signal enemy_killed
 
 func _ready() -> void:
 	level = 1
-	lives = 50
-	max_enemies = 10
+	lives = 3
+	max_enemies = 30
 	current_enemies = max_enemies
 	$HUD_Level1/LivesLabel.text = "X " + str(lives)
 	$HUD_Level1/EnemiesLabel.text = "X " + str(current_enemies)
@@ -20,12 +19,17 @@ func _ready() -> void:
 func _on_enemy_killed() -> void:
 	current_enemies -= 1
 	$HUD_Level1/EnemiesLabel.text = "X " + str(current_enemies)
+	
+	# Allow transition only when all enemies are killed
+	if current_enemies <= 0:
+		can_transition = true
 
 func _on_enemy_spawner_hit_p() -> void:
 	lives -= 1
 	$HUD_Level1/LivesLabel.text = "X " + str(lives)
-	emit_signal("player_damaged")  # Emit signal when player is hit
+	emit_signal("player_damaged")
 
-
-func _on_door_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+func _on_door_to_lvl_2_body_entered(body: Node2D) -> void:
+	# Only change scene if all enemies are killed
+	if can_transition:
+		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
